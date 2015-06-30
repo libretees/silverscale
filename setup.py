040e93f4efe3b61477e7ad9135f8ec653c395802
Jared Contrascere <jcontra@gmail.com>
2015-06-30 03:04:49 +0000

    Submit 'silverscale' package to PyPI.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name = 'silverscale',
+    version = '0.1',
+    description = 'Retrieve data from HID USB Scales.',
+    author = 'Jared Contrascere',
+    author_email = 'jcontra@gmail.com',
+    url = 'https://github.com/libretees/silverscale',
+    install_requires=['Cython==0.22.1',
+                      'hidapi==0.7.99-6',],
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
