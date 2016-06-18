01fa50f87121015a5ea7fd9ad503a4c9fb056a50
Jared Contrascere <jcontra@gmail.com>
2016-06-18 18:01:04 -0400

    Add list of USB Scale Vendor/Device IDs.

---
+++ silverscale/usb_ids.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+USB_IDS = {
+    0x0b67: {
+        'vendor': 'Fairbanks Scales',
+        'devices': {
+            0x555e: 'SCB-R9000'
+        }
+    },
+    0x0eb8: {
+        'vendor': 'Mettler Toledo',
+        'devices': {
+            0x2200: 'Ariva Scale',
+            0xf000: 'PS60 Scale'
+        }
+    },
+    0x0922: {
+        'vendor': 'Dymo-CoStar Corp.',
+        'devices': {
+            0x8003: '1772057 Digital Postal Scale',
+            0x8004: 'M25 Digital Postal Scale'
+        }
+    },
+    0x1446: {
+        'vendor': 'X.J.GROUP',
+        'devices': {
+            0x6a73: 'Stamps.com Model 510 5LB Scale',
+            0x6a78: 'DYMO Endicia 75lb Digital Scale'
+        }
+    },
+    0x2233: {
+        'vendor': 'RadioShack Corporation',
+        'devices': {
+            0x6323: 'USB Electronic Scale'
+        }
+    },
+    0x2474: {
+        'vendor': 'Stamps.com',
+        'devices': {
+            0x0550: 'Stainless Steel 5 lb. Digital Scale',
+            0x3550: 'Stainless Steel 35 lb. Digital Scale'
+        }
+    },
+    0x6096: {
+        'vendor': 'SANFORD',
+        'devices': {
+            0x0158: 'Dymo 10 lb USB Postal Scale'
+        }
+    },
+    0x7b7c: {
+        'vendor': 'XM',
+        'devices': {
+            0x0100: 'Elane UParcel 30lb'
+        }
+    }
+}
