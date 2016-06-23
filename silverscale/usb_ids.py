f1f89110e70b5ec65c1e83be3511850bab8ac8a6
Jared Contrascere <jcontra@gmail.com>
2016-06-22 20:33:20 -0400

    Add additional scales to list of USB IDs.

--- silverscale/usb_ids.py
+++ silverscale/usb_ids.py
@@ -18,8 +18,10 @@ USB_IDS = {
     0x0922: {
         'name': 'Dymo-CoStar Corp.',
         'devices': {
-            0x8003: 'M10 Digital Postal Scale',
-            0x8004: 'M25 Digital Postal Scale'
+            0x8003: 'M10 10 lb Digital Postal Scale',
+            0x8004: 'M25 25 lb Digital Postal Scale',
+            0x8007: 'S100 100 lb Digital Shipping Scale',
+            0x8009: 'S250 250 lb Digital Shipping Scale'
         }
     },
     0x1446: {
