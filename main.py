9bf80e812509af369c5efca458274026de776e60
Jared Contrascere <jcontra@gmail.com>
2016-06-27 15:43:41 -0400

    Process USB report data into its associated type.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from silverscale.device import USBDeviceManager
+from silverscale.reports import REPORT_TYPES
 
 device_manager = USBDeviceManager()
 
@@ -10,8 +11,10 @@ for device in device_manager.devices:
     print(device.manufacturer, device.product, device.serial_number)
 
     for i in range(3):
-        data = device.read(6)
-        print(data)
+        report_data = device.read(6)
+        if report_data:
+            report = REPORT_TYPES[report_data[0]](report_data)
+            print(report)
 
 device_manager.close()
 
